c400ba6bb5f9cf78c7c90e20e5118e2f3b63d945
nkidmano <nobudy55@gmail.com>
2018-04-08 11:48:41 +0700

    building register and input validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express       = require("express");
 const mongoose      = require("mongoose");
+const Joi           = require("joi");
 const bodyParser    = require("body-parser");
 const passport      = require("passport");
 const LocalStrategy = require("passport-local");
@@ -10,6 +11,7 @@ const User          = require("./models/user");
 // Init app
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -27,7 +29,7 @@ passport.deserializeUser(User.deserializeUser());
 
 // Connect to db
 mongoose.connect("mongodb://localhost/cake-store");
-var db = mongoose.connection;
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log("Connected to MongoDB");
@@ -42,7 +44,7 @@ db.once('open', function() {
 //         image: "/test-1.jpg", 
 //         description: "Bread and cummie, simple but delicous and rich af",
 //         price: 2.35
-//     }, function(err, cake){
+//     }, (err, cake) => {
 //         if(err){
 //             console.log(err)
 //         } else {
@@ -52,43 +54,67 @@ db.once('open', function() {
 //     }
 // )
 
-app.get("/menu", function(req, res){
-    Cake.find({}, function(err, allCakes){
+app.get("/menu", (req, res) => {
+    Cake.find({}, (err, allCakes) => {
         if(err){
+            res.status(404).send("Website are downed");
             console.log(err)
         } else {
-            res.render("menu",{cakes:allCakes});
+            res.render("menu", {cakes:allCakes});
         }
     });
 });
 
-app.get("/login", function(req, res){
+// app.get("/menu/:_id", (req, res) => {
+//     Cake.find(c => c.id === parseInt(req.params.id), (err, cake) => {
+//         if(err) {
+//             res.status(404).send("Cannot find the cake with the given ID");
+//         } else {
+//             res.render()
+//         }
+//     });
+// });
+
+app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", (req, res) => {
     res.render("register");
 });
 
-app.post("/register", function(req, res){
-    var newUser = new User({name: req.body.email});
-    User.register(newUser, req.body.password, function(err, user){
+app.post("/register", (req, res) => {
+    const schema = {
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        password: Joi.string().min(6).required()
+    };
+
+    const validateResult = Joi.validate(req.body, schema);
+    if(validateResult.error) {
+        res.status(400).send(validateResult.error);
+        return;
+    }
+
+    const newUser = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    User.create(newUser, (err, user) => {
         if(err){
             console.log(err)
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function(){
+        } else {
+            console.log(user);
             res.redirect("/menu");
-        });
+        }
     });
 });
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     res.redirect("/menu");
 });
 
-
-
-app.listen(3000, function(){
-    console.log("SERVER STARTED");
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
