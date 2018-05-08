eb465c04ec6391b7f194df11c2b1eb04916443ca
nkidmano <nobudy55@gmail.com>
2018-05-08 10:28:57 +0700

    added edit,new view/routes for cake

--- routes/cakes.js
+++ routes/cakes.js
@@ -5,21 +5,39 @@ const _ = require('lodash');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const cakes = await Cake.find().sort('name');
+  const cakes = await Cake.find();
   res.render('cakes/index', { 
     title: 'Menu',
     cakes: cakes
   });
 });
 
-router.post('/', async (req, res) => {
+router.get('/new', (req, res) => {
+  res.render('cakes/new', {
+    title: 'Add cake',
+  })
+});
+
+router.get('/:id/edit', async (req, res) => {
+  const cake = await Cake.findById(req.params.id);
+  res.render('cakes/edit', { 
+    title: 'Edit',
+    cake: cake
+  });
+});
+
+router.post('/new', async (req, res) => {
   const { error } = validate(req.body)
   if (error) res.status(400).send(error.details[0].message);
 
   const cake = new Cake(_.pick(req.body, ['name', 'image', 'description', 'price']));
+
+  const path = './images/'; // hard code path for image
+  cake.image = `${path}${req.body.image}`; // redefine image path, html can't get accurate path
+
   await cake.save();
 
-  res.send(cake);
+  res.redirect('/cakes');
 });
 
 router.put('/:id', async (req, res) => {
@@ -31,10 +49,16 @@ router.put('/:id', async (req, res) => {
     'image',
     'description', 
     'price'
-  ]), { new: true });
+  ]));
+
+  const path = './images/'; // hard code path for image
+  cake.image = `${path}${req.body.image}`; // redefine image path, html can't get accurate path
+  
+  await cake.save();
+
   if (!cake) return res.status(404).send('The cake with given ID was not found.');
 
-  res.send(cake);
+  res.redirect('/cakes');
 });
 
 router.delete('/:id', async (req, res) => {
